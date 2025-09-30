5e2d1968931616c98f276662b954e8cdbee12a13
nawied <nawied.syed@gmx.de>
2025-09-30 12:10:38 +0200

    feat: Enhance About section with content and styling; update routing and Hero component layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HeroComponent },
   { path: '', component: NavbarComponent },
+  { path: '', component: AboutComponent },
 ];
