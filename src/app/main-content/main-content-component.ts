7662d54714039dc102a0638eb470a7a8668d1eaa
nawied <nawied.syed@gmx.de>
2025-10-04 11:26:58 +0200

    feat: Add Testimonials component with layout and styling; update main content structure

--- src/app/main-content/main-content-component.ts
+++ src/app/main-content/main-content-component.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @Component({
   selector: 'app-main-content-component',
   standalone: true,
-  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, TestimonialsComponent],
   templateUrl: './main-content-component.html',
   styleUrl: './main-content-component.scss',
 })
