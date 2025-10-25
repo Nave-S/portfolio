d1352ca17f16b4a8ce9eca06c1d838ed474a20d7
nawied <nawied.syed@gmx.de>
2025-10-25 12:10:02 +0200

    feat: add legal notice component and update routes
    - Added LegalNoticeComponent with corresponding HTML, SCSS, and spec files.
    - Updated app.routes.ts to include a route for the legal notice.
    - Translated imprint content to use ngx-translate for better localization.
    - Updated footer to include a link to the legal notice.
    - Added German and English translations for legal notice and imprint sections.

---
+++ src/app/legal-notice/legal-notice.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { LanguageService } from '../services/language.service';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-legal-notice',
+  standalone: true,
+  imports: [NavbarComponent, TranslateModule],
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss',
+})
+export class LegalNoticeComponent {
+  constructor(private lang: LanguageService) {}
+  setLang(lang: 'en' | 'de') {
+    this.lang.use(lang);
+  }
+
+  get current() {
+    return this.lang.current();
+  }
+}
+
